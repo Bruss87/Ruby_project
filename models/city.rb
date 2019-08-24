e469a7f7e54ec81d3733369d248f856bf8737fa0
Karolina Kaczmarska <kaczmarska.kar@gmail.com>
2019-08-24 14:05:00 +0100

    tested attributes for the city class

--- models/city.rb
+++ models/city.rb
@@ -2,19 +2,23 @@ require_relative("../db/sql_runner")
 
 class City
 
+attr_accessor :if_visited
+attr_reader :name, :country_id, :id
+
 def initialize(options)
   @name = options['name']
   @if_visited = options['if_visited']
-  @country_id = options['country_id']
+  @country_id = options['country_id'].to_i
   @id = options['id'].to_i if options['id']
 end
 
-def save
+def save()
   sql = "INSERT INTO cities (name, if_visited, country_id)
   values = ($1, $2, $3) RETURNING id"
   city = SqlRunner.run(sql, values)
   @id = city[0]['id'].to_i
 end
+
 
 
 
--- models/specs/city_spec.rb
+++ models/specs/city_spec.rb
@@ -1,10 +1,30 @@
 require("minitest/autorun")
 require_relative("../city")
 
-class TestPizzaOrder < MiniTest::Test
+class TestCity < MiniTest::Test
 
+def setup()
+  options = {
+    "name" => "Bergen",
+    "if_visited" => "true", "country_id" => 1,
+    "id" => 1
+  }
+   @city = City.new(options)
+end
 
+def test_name()
+  result = @city.name()
+  assert_equal("Bergen", result)
+end
 
+def test_if_visited()
+result = @city.if_visited()
+assert_equal("true", result)
+end
 
-  
+def test_country_id
+  result = @city.country_id
+  assert_equal(1, result)
+end
+
 end
